3e2b5e1466839211279716abf6c7fca064d6a3af
Hitension <112086126+Edikkan@users.noreply.github.com>
2026-03-02 15:37:52 +0100

    Add backend/src/validators/authValidator.js

---
+++ backend/src/validators/authValidator.js
@@ -0,0 +1,39 @@
+const { body } = require('express-validator');
+
+exports.registerValidator = [
+  body('firstName')
+    .trim()
+    .notEmpty().withMessage('First name is required')
+    .isLength({ min: 2, max: 50 }).withMessage('First name must be between 2 and 50 characters'),
+
+  body('lastName')
+    .trim()
+    .notEmpty().withMessage('Last name is required')
+    .isLength({ min: 2, max: 50 }).withMessage('Last name must be between 2 and 50 characters'),
+
+  body('email')
+    .trim()
+    .notEmpty().withMessage('Email is required')
+    .isEmail().withMessage('Please provide a valid email')
+    .normalizeEmail(),
+
+  body('password')
+    .notEmpty().withMessage('Password is required')
+    .isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+    .matches(/\d/).withMessage('Password must contain a number'),
+
+  body('phone')
+    .optional()
+    .isMobilePhone('any').withMessage('Please provide a valid phone number'),
+];
+
+exports.loginValidator = [
+  body('email')
+    .trim()
+    .notEmpty().withMessage('Email is required')
+    .isEmail().withMessage('Please provide a valid email')
+    .normalizeEmail(),
+
+  body('password')
+    .notEmpty().withMessage('Password is required'),
+];
